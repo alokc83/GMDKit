f0f3f6542e29034b65d1704a247616da8ed00eee
Alok Choudhary <ac687j@att.com>
2016-06-11 18:21:09 -0400

    Initial Commit

---
+++ GMDKit/GMDKit.h
@@ -0,0 +1,19 @@
+//
+//  GMDKit.h
+//  GMDKit
+//
+//  Created by Alok Choudhary on 6/11/16.
+//  Copyright © 2016 Mt. Aden Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMDKit.
+FOUNDATION_EXPORT double GMDKitVersionNumber;
+
+//! Project version string for GMDKit.
+FOUNDATION_EXPORT const unsigned char GMDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMDKit/PublicHeader.h>
+
+
